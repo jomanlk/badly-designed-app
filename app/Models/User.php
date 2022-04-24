ca4c7c066be776ab49712642ff7cd5fdc9cc3b75
JohnP <john@rentman.nl>
2022-04-24 22:33:42 +0530

    Create models

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+ 
+    public function hearts() {
+        return $this->hasMany(Heart::class);
+    }
+
+    public function messages() {
+        return $this->hasMany(Message::class);
+    }
 }
